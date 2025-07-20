ae15c14b57bb671aea848a6487f3abba6d8c1428
Shashwat Singh <shashawat15@gmail.com>
2025-07-20 22:14:30 +0530

    Add files via upload

---
+++ equality.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int x, y;
+    cout << "x: ";
+    cin >> x;
+    cout << "y: ";
+    cin >> y;
+
+    bool flag = true;
+    bool flag1 = false;
+
+    x == y ? cout << flag : cout << flag1;
+
+    return 0;
+}
